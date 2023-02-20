c9e92547b8af88a12e5f4de371f5678d431a4904
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-02-20 21:53:41 +0100

    Enabling contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { AiOutlineMail } from "react-icons/ai";
+
 import "./Contact.css";
 
 const Contact = () => {
@@ -7,24 +9,36 @@ const Contact = () => {
     <>
       <div className="contact_container">
         <div className="contact_text">
-          <h1>Contact Me</h1>
+          <h1>
+            Let's Chat.
+            <br />
+            Tell me about your project
+          </h1>
           <p>
             Interested in working together on a MERN stack project? Reach out
             and let's discuss how we can collaborate.
           </p>
+          <p>Let's create something </p>
+          <p className="mail">
+            <span>
+              <AiOutlineMail />
+            </span>
+            <span className="mail_se">frontprogramer@gmail.com</span>
+          </p>
         </div>
         <form className="contact_form">
-          <div className="form_control ">
-            <input required="required" type="text" />
-            <span>Name</span>
-            <i></i>
+          <h1>Send Me a Message🚀</h1>
+          <div className="flex">
+            <div className="form_control ">
+              <input required="required" type="text" />
+              <span>Name</span>
+            </div>
+            <div className="form_control ">
+              <input required="required" type="email" />
+              <span>Email</span>
+            </div>
           </div>
 
-          <div className="form_control ">
-            <input required="required" type="email" />
-            <span>Email</span>
-            <i></i>
-          </div>
           <textarea placeholder="Message" rows={30}></textarea>
           <button className="send_button">Send</button>
         </form>
